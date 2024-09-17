4ca71557f07a90da790c18c900001653d1c74116
karan <karan.pratap@mobulous.com>
2024-09-17 17:53:35 +0530

    add heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import CreateRoom from "../components/CreateRoom";
 const Home = () => {
   return (
     <div className="flex justify-center items-center h-[100vh]">
+      <h1>Ved App</h1>
       <CreateRoom />
     </div>
   );
